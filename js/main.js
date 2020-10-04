f23351553dfca722449a6b4b37943b425724fbea
Valeria <valery0007@gmail.com>
2020-10-04 12:39:07 +0200

    allproducts.html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
-    //dropdown options
-    $(".dropdown-toggle").dropdown({
-        offset: -90,
-        flip: false,
-    });
+  //dropdown options
+  $(".dropdown-toggle").dropdown({
+    offset: -90,
+    flip: false,
+  });
 
-    //tooltip
-    $('[data-toggle="tooltip"]').tooltip({
-        delay: {
-            show: 2000,
-            hide: 1000
-        },
-    });
+  //tooltip
+  $('[data-toggle="tooltip"]').tooltip({
+    delay: {
+      show: 1000,
+      hide: 1000,
+    },
+    html: true,
+    placement: 'bottom',
+    offset: 70
+  });
+  
+  //popover
+  $('[data-toggle="popover"]').popover({
+      
+  });
+  
 });
