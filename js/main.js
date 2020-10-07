3a1cd8ff2f7c91c9eab9c44dce237311d6b2b277
Valeria <valery0007@gmail.com>
2020-10-07 12:09:04 +0200

    popovers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let tooltipButton = document.getElementById('tooltipButton');
+
 $(document).ready(function () {
   //dropdown options
   $(".dropdown-toggle").dropdown({
@@ -12,14 +14,30 @@ $(document).ready(function () {
       hide: 1000,
     },
     html: true,
-    placement: 'bottom',
-    offset: 70
+    placement: "bottom",
+    offset: 70,
   });
-  
+
   //pop over
   $('[data-toggle="popover"]').popover({
-      trigger: 'hover',
-      html: true
+    trigger: "hover",
+    html: true,
   });
-  
 });
+
+//popovers
+
+function viewPopovers() {$('#pop1').popover('toggle');}
+
+function showTooltips() {
+  $('[data-toggle="tooltip"]').tooltip('toogle');
+}
+
+$('[data-toggle="tooltip"]').on('show.bs.tooltip', function() {
+  tooltipButton.innerHTML = 'Hide Tooltips';
+})
+
+$('[data-toggle="tooltip"]').on('hide.bs.tooltip', function() {
+  tooltipButton.innerHTML = 'Show Tooltips';
+})
+
